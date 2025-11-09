ecb704936c47eb3d4928a180a480d58ee4408ffc
SDH2002 <sdhsdh2002@gmail.com>
2025-11-09 15:01:52 +0900

    보스 걷는 애니메이션 추가

--- Assets/script/Bossactive.cs
+++ Assets/script/Bossactive.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 public class Bossactive : MonoBehaviour
 {
     GameObject player;
-    public float speed;
-    public float tracestart;
-
-    
+    SpriteRenderer bossrenderer;
+    Animator bossanimator;
+    public float speed; //속도
+    public float tracestart; //추적 시작 거리
+    public float face; //방향
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         this.player = GameObject.Find("Player");
+        bossrenderer = GetComponent<SpriteRenderer>();
+        bossanimator = GetComponent<Animator>();
+        face = 1;
     }
 
     /*public void Selector()
@@ -29,14 +33,27 @@ public class Bossactive : MonoBehaviour
     {
         Vector2 pb = transform.position;//보스의 위치
         Vector2 pp = this.player.transform.position;//플레이어의 위치
-        Vector2 tracedir = pp - pb;
-        float d = tracedir.magnitude;
-        if (d >= tracestart)
+        Vector2 tracedir = pp - pb; //보스에서 플레이어를 향하는 벡터
+        if (tracedir.x < 0) //스프라이트 뒤집기
         {
+            bossrenderer.flipX = true;
+        }
+        else
+        {
+            bossrenderer.flipX = false;
+        }
+        float d = tracedir.magnitude;
+        if (d >= tracestart) //추격 시작
+        {
+            this.bossanimator.SetBool("BossStopTrig", false);
+            this.bossanimator.SetBool("BossWalkTrig", true);
+
             transform.Translate(tracedir * speed * Time.deltaTime);
         }
         else
         {
+            this.bossanimator.SetBool("BossWalkTrig", false);
+            this.bossanimator.SetBool("BossStopTrig", true);
         }
         
     }
