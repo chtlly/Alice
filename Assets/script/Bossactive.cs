88a887136e0ad540c7225cd6fc63812b3bff337c
SDH2002 <sdhsdh2002@gmail.com>
2025-11-14 17:26:46 +0900

    보스 걷는 애니메이션 수정

--- Assets/script/Bossactive.cs
+++ Assets/script/Bossactive.cs
@@ -8,7 +8,11 @@ public class Bossactive : MonoBehaviour
     Animator bossanimator;
     public float speed; //속도
     public float tracestart; //추적 시작 거리
+    public float attackstart;
     public float face; //방향
+
+    public bool IsTracing = false;
+    public bool IsAttacking = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,13 +29,13 @@ public class Bossactive : MonoBehaviour
 
     public void BasicAttack()
     {
-
+        Debug.Log("기본공격중");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pb = transform.position;//보스의 위치
+        Vector2 pb = this.transform.position;//보스의 위치
         Vector2 pp = this.player.transform.position;//플레이어의 위치
         Vector2 tracedir = pp - pb; //보스에서 플레이어를 향하는 벡터
         if (tracedir.x < 0) //스프라이트 뒤집기
@@ -43,18 +47,23 @@ public class Bossactive : MonoBehaviour
             bossrenderer.flipX = false;
         }
         float d = tracedir.magnitude;
-        if (d >= tracestart) //추격 시작
+        if (d >= tracestart && !IsTracing)
         {
-            this.bossanimator.SetBool("BossStopTrig", false);
-            this.bossanimator.SetBool("BossWalkTrig", true);
-
-            transform.Translate(tracedir * speed * Time.deltaTime);
+            Debug.Log("추격 시작");
+            IsTracing = true;
+            this.bossanimator.SetTrigger("BossWalkTrig");
+            this.transform.Translate(tracedir * speed * Time.deltaTime);
         }
-        else
+        else if (d >= 1 && IsTracing)
         {
-            this.bossanimator.SetBool("BossWalkTrig", false);
-            this.bossanimator.SetBool("BossStopTrig", true);
+            Debug.Log("추격 중");
+            this.transform.Translate(tracedir * speed * Time.deltaTime);
         }
-        
+        else if (d <= 1 && IsTracing)
+        {
+            Debug.Log("추격 중단");
+            IsTracing = false;
+            this.bossanimator.SetTrigger("BossIdleTrig");
+        }
     }
 }
